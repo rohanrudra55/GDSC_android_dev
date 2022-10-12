180f7ba9c278032b93d6717303f1eeaff9f231dc
Rohan Rudra <rohanrudra55@gmail.com>
2022-10-12 15:54:29 +0530

    Task 3 done

--- ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.composearticle
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.AppBarDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +31,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AppViewPage(h1 = getString(R.string.Header1), b1 = getString(R.string.Body1), b2 = getString(R.string.Body2))
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppBackground(){
+    val image = painterResource(R.drawable.bg_compose_background)
+    androidx.compose.foundation.Image(
+        painter = image,
+        contentDescription = "Background Image",
+        modifier = Modifier.fillMaxWidth(),
+//        contentScale = ContentScale.Crop
+        )
 }
 
+@Composable
+fun AppViewPage(h1 : String, b1 : String, b2 : String){
+    Column {
+        AppBackground()
+        Text(
+            text = h1,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = b1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(all = 16.dp)
+        )
+        Text(
+            text = b2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(all = 16.dp)
+            )
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        AppViewPage("H1", b1 = "Body", b2 = "Body")
     }
 }
